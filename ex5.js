e394bba091782d635db59d8a0cd4078efce9889b
Rubens <rubenskarvalhoveloso@gmail.com>
2024-12-19 19:19:48 -0300

    Exércio 5

--- ex5.js
+++ ex5.js
@@ -1,7 +1,11 @@
-let temperatura = prompt ("Temperatura:")
+let temperatura = Number (prompt ("Temperatura:"))
 
-if (temperatura = "celsius") {
-    let fahremreit = (temperatura * 1.8) + 32
+let  escalaTermica = prompt ("Para qual escala térmica deseja fazer a conversão?")
+
+if (escalaTermica === "celsius") {
+    let celsius = Number ((temperatura -32) / 1.8);
+    console.log ("conversão para celsius:",celsius,"°C");
 } else {
-    
+    let fahremreit = Number ((temperatura * 1.8) + 32);
+    console.log ("Conversão para fahremreit:",fahremreit,"°F");
 }
