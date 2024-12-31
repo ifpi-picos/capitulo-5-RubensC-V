c071efbc96bf28d156365b5a531554ca5ee43b10
Rubens <rubenskarvalhoveloso@gmail.com>
2024-12-30 22:32:38 -0300

    Exércicio 10

--- ex10.js
+++ ex10.js
@@ -8,4 +8,6 @@ i += 2;
 } 
 
 /* Se a colocar a adição da variavel dentro do console.log, vai contar a partir de 2 e não de 0 e 
-vai contar dois números a mais a cima do valor da variável numero. por isso, vai estar errado.*/
+vai contar dois números a mais a cima do valor da variável numero. por isso, vai estar errado.
+i += 2 no console.log: Incrementa antes de exibir, omitindo o valor inicial.
+Correção: Separe o incremento da exibição para ter controle sobre o comportamento do loop.*/
