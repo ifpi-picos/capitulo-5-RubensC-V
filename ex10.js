8c74b5781affe011cf1e56ab4ad70919053f0413
Rubens <rubenskarvalhoveloso@gmail.com>
2024-12-30 22:26:45 -0300

    Exércicio 10

---
+++ ex10.js
@@ -0,0 +1,11 @@
+let i = 0;
+
+let numero = Number (prompt ("Digite um número positivo:"))
+
+while (i < numero){
+console.log (i);
+i += 2;
+} 
+
+/* Se a colocar a adição da variavel dentro do console.log, vai contar a partir de 2 e não de 0 e 
+vai contar dois números a mais a cima do valor da variável numero. por isso, vai estar errado.*/
